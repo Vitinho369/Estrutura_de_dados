3133a60ea71bcf74e6d29103a172ed1b072af4cc
Vitinho369 <vitinho21.2005@gmail.com>
2024-03-13 22:36:02 -0300

    Resolução da questão 6 lista 2

---
+++ src/Lista2/Questao_6.js
@@ -0,0 +1,15 @@
+const inverterFila = (fila,filaInvertida) =>{
+    let filaN = fila;
+    let guardaFront;
+
+    if(filaN.length() > 0){
+        guardaFront = filaN.front();
+        filaN.dequeue();
+        inverterFila(filaN, filaInvertida);
+        filaInvertida.enqueue(guardaFront);
+    }
+
+    return filaInvertida;
+};
+
+export default inverterFila;
